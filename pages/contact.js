83d283ee402c969b5451e427d926f79c25993382
akhilthej <67720804+akhilthej@users.noreply.github.com>
2024-01-23 21:59:20 +0530

    1

--- pages/contact.js
+++ pages/contact.js
@@ -66,19 +66,19 @@ const ContactForm = () => {
 
     {/*contact form*/}
     <section className="bg-gray-200 pt-20">
-        <div class="relative flex items-top justify-center  sm:items-center sm:pt-0">
-          <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class=" overflow-hidden">
-              <div class="grid grid-cols-1 md:grid-cols-2">
-                <div class="p-6 mr-2   sm:rounded-lg">
-                  <h1 class="text-center sm:text-left text-4xl sm:text-5xl text-black font-extrabold tracking-tight">
+        <div className="relative flex items-top justify-center  sm:items-center sm:pt-0">
+          <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div className=" overflow-hidden">
+              <div className="grid grid-cols-1 md:grid-cols-2">
+                <div className="p-6 mr-2   sm:rounded-lg">
+                  <h1 className="text-center sm:text-left text-4xl sm:text-5xl text-black font-extrabold tracking-tight">
                     Get in Touch
                   </h1>
-                  <p class="text-center sm:text-left text-normal text-lg sm:text-2xl font-medium text-black mt-2">
+                  <p className="text-center sm:text-left text-normal text-lg sm:text-2xl font-medium text-black mt-2">
                     Fill in the form to start a conversation
                   </p>
 
-                  <div class="flex items-center mt-8 text-black">
+                  <div className="flex items-center mt-8 text-black">
                     <svg
                       fill="none"
                       stroke="currentColor"
@@ -86,7 +86,7 @@ const ContactForm = () => {
                       stroke-linejoin="round"
                       stroke-width="1.5"
                       viewBox="0 0 24 24"
-                      class="w-8 h-8 text-gray-500"
+                      className="w-8 h-8 text-gray-500"
                     >
                       <path
                         stroke-linecap="round"
@@ -101,13 +101,13 @@ const ContactForm = () => {
                         d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                       />
                     </svg>
-                    <div class="ml-2 text-sm sm:text-base text-md tracking-wide font-semibold w-">
+                    <div className="ml-2 text-sm sm:text-base text-md tracking-wide font-semibold w-">
                       Shivaji Palem, Maddilapalem,
                       <br /> Visakhapatnam, Andhra Pradesh 530017
                     </div>
                   </div>
 
-                  <div class="flex items-center mt-4 text-black">
+                  <div className="flex items-center mt-4 text-black">
                     <svg
                       fill="none"
                       stroke="currentColor"
@@ -115,7 +115,7 @@ const ContactForm = () => {
                       stroke-linejoin="round"
                       stroke-width="1.5"
                       viewBox="0 0 24 24"
-                      class="w-8 h-8 text-gray-500"
+                      className="w-8 h-8 text-gray-500"
                     >
                       <path
                         stroke-linecap="round"
@@ -124,12 +124,12 @@ const ContactForm = () => {
                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                       />
                     </svg>
-                    <div class="ml-4 text-sm sm:text-base tracking-wide font-semibold w-40">
+                    <div className="ml-4 text-sm sm:text-base tracking-wide font-semibold w-40">
                       (+91) 970 489 9400
                     </div>
                   </div>
 
-                  <div class="flex items-center mt-4 text-black">
+                  <div className="flex items-center mt-4 text-black">
                     <svg
                       fill="none"
                       stroke="currentColor"
@@ -137,7 +137,7 @@ const ContactForm = () => {
                       stroke-linejoin="round"
                       stroke-width="1.5"
                       viewBox="0 0 24 24"
-                      class="w-8 h-8 text-gray-500"
+                      className="w-8 h-8 text-gray-500"
                     >
                       <path
                         stroke-linecap="round"
@@ -146,7 +146,7 @@ const ContactForm = () => {
                         d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                       />
                     </svg>
-                    <div class="ml-4 text-sm sm:text-base tracking-wide font-semibold w-40">
+                    <div className="ml-4 text-sm sm:text-base tracking-wide font-semibold w-40">
                       admin@cyberspacedigital.in
                     </div>
                   </div>
